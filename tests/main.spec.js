d447d423ea21c593b9e06f7991a160c0ed1d44cf
Willian Justen <willianjustenqui@gmail.com>
2017-07-02 13:46:58 -0300

    Test Hello World code

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,7 +1,14 @@
-import { expect } from 'chai';
+const expect = require('chai').expect;
 
-describe('Main', () => {
-  it('should do what...', () => {
-    expect(true).to.be.true;
+const exec = require('child_process').exec;
+const btcConverter = './src/main.js';
+
+describe('Main CLI', () => {
+  it('should return Hello World', (done) => {
+    exec(btcConverter, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal('Hello World!');
+      done();
+    });
   });
 });
