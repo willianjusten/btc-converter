44ae7e9766a8d41b214acaa22add7900f327f346
Willian Justen <willianjustenqui@gmail.com>
2017-07-02 17:21:21 -0300

    Using the API

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -1,5 +1,12 @@
+const request = require('request');
+
 function convertBTC(currency = 'USD', amount = 1) {
-  return `${amount} BTC to ${currency} = 2000.00`;
+  const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
+
+  request(url, (error, response, body) => {
+    let apiResponse = JSON.parse(body);
+    console.log(`${amount} BTC to ${currency} = ${apiResponse.price}`);
+  });
 }
 
 module.exports = convertBTC;
--- tests/ConvertBTC.spec.js
+++ tests/ConvertBTC.spec.js
@@ -1,14 +1,56 @@
+const nock = require('nock');
 const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 const expect = chai.expect;
+
+chai.use(sinonChai);
 
 const convertBTC = require('../src/ConvertBTC');
 
 describe('ConvertBTC', () => {
-  it('should return USD as currency and 1 as amount default', () => {
-    expect(convertBTC()).to.be.equal('1 BTC to USD = 2000.00');
+
+  let consoleStub;
+
+  const responseMock = {
+    "success": true,
+    "time": "2017-07-02 18:51:29",
+    "price": 2490.78
+  };
+
+  beforeEach(() =>{
+    consoleStub = sinon.stub(console, 'log');
   });
 
-  it('should return BRL as currency and 10 as amount when defined', () => {
-    expect(convertBTC('BRL', 10)).to.be.equal('10 BTC to BRL = 2000.00');
+  afterEach( () => {
+    console.log.restore();
+  });
+
+  it('should use currency USD and 1 as amount default', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'USD', amount: 1})
+      .reply(200, responseMock);
+
+    convertBTC();
+
+    setTimeout(() =>{
+      expect(consoleStub).to.have.been.calledWith('1 BTC to USD = 2490.78');
+      done();
+    }, 300)
+  });
+
+  it('should use currency USD and 10 as amount', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'USD', amount: 10})
+      .reply(200, responseMock);
+
+    convertBTC('USD', 10);
+
+    setTimeout(() =>{
+      expect(consoleStub).to.have.been.calledWith('10 BTC to USD = 2490.78');
+      done();
+    }, 300)
   });
 });
